cdd16dfeb224205434b3ad085ddb0cd640b7dcda
Siphesihle Mbuyisa <siphesihlembuyisa@Sphes-MacBook-Air.local>
2023-03-12 20:08:50 +0200

    theme change + links change

--- components/link.js
+++ components/link.js
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Link({Logo, Name, Link}) {
     return (
         <button onClick={() => {event.preventDefault();window.open(Link, "_blank");}} className='w-[300px]'>
-            <div className='bg-[#212121] rounded-full flex justify-center items-center p-3 my-3 transition-all hover:scale-110'>
-              <Image src={Logo} className='w-4 mx-1'/>
+            <div className='border border-black shadow-xl rounded-full flex justify-center items-center p-3 my-3 transition-all lg:hover:scale-110 lg:hover:shadow-2xl'>
+              {/* <Image src={Logo} className='w-4 mx-1'/> */}
               <p>{Name}</p>
             </div>
           </button>
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Profile from '@/public/images/profile.jpeg'
+import Profile from '@/public/images/profile.png'
 import Link from '@/components/link.js'
 import { useState } from 'react'
 
 import TikTok from '@/public/images/tiktok.png'
 import YouTube from '@/public/images/youtube.webp'
 import Instagram from '@/public/images/instagram.png'
+import Discord from '@/public/images/discord.png'
 
 export default function Home() {
 
@@ -18,7 +19,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div class='bg-gradient-to-r from-black to-[#101010]  h-screen flex flex-col justify-center items-center text-white'>
+      <div class='h-screen flex flex-col justify-center items-center text-[#0A0A0A]'>
       <Image
             src={Profile}
             className='w-24 rounded-full m-4 shadow-lg transition-all'
@@ -27,11 +28,11 @@ export default function Home() {
         <h2 className='text-[18px] m-1'>Video Editor, Coder</h2>
 
       <div class="m-5 w-[300px]">
-        <Link Logo={TikTok} Name={'TikTok'} Link={"https://www.tiktok.com/@empire_cord"}/>
-        <Link Logo={YouTube} Name={'YouTube'} Link={"https://www.youtube.com/@PositiveMusculinity"}/>
-        <Link Logo={Instagram} Name={'Instagram'} Link={"https://www.instagram.com/sphe.exe/"}/>
+        <Link Logo={TikTok} Name={'TikTok'} Link={"https://www.tiktok.com/@sphe.exe"}/>
+        <Link Logo={TikTok} Name={'Visit my website'} Link={"https://sphe.vercel.app/"}/>
+        <Link Logo={Discord} Name={'Join my discord community'} Link={"https://discord.gg/PBpeEV8cmC"}/>
       </div>
-      <h1 className='text-[25px] selection:bg-black'>📬</h1>
+      <h1 className='text-[25px] selection:bg-transparent'>📬</h1>
   </div>
     </>
   )
