cf366d7bbf6f0e1598494de8847fbddabfaca057
Siphesihle Mbuyisa <siphesihlembuyisa@Sphes-MacBook-Air.local>
2023-03-23 15:47:01 +0200

    animation remove

--- pages/index.js
+++ pages/index.js
@@ -14,20 +14,6 @@ import EmailList from '@/components/EmailList'
 
 export default function Home() {
 
-  // Home Page Animation
-  const profileAnimation = useSpring({
-    from: { opacity: 0, transform: 'scale(0)' },
-    to: { opacity: 1, transform: 'scale(1)' },
-    delay: 10,
-  });
-
-  // Descripiton Animation
-  const linksAnimation = useSpring({
-    from: { opacity: 0, transform: 'translateY(130px)' },
-    to: { opacity: 1, transform: 'translateY(0)' },
-    delay: 550,
-  });
-
   return (
     <>
       <Head>
@@ -37,12 +23,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div class='h-screen flex flex-col justify-center items-center text-[#0A0A0A]'>
-      <animated.div style={profileAnimation}>
+      <div>
         <Image
               src={Profile}
               className='w-24 rounded-full m-4 shadow-lg transition-all'
             />
-      </animated.div>
+      </div>
       
       <h1 className='font-bold text-lg'>@sphe.exe</h1>
       <h2 className='text-[18px] mb-2'>Video Editor, Coder</h2>
